25ea0e7c2f7066bf095edc3662058b2fbbde2fbd
Krithick Ganesh <krithickganesh1637@gmail.com>
2025-10-25 15:56:19 +0530

    Add build configuration and development tools

---
+++ next.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  poweredByHeader: false,
+  reactStrictMode: true,
+  experimental: {
+    turbo: {
+      rules: {
+        '*.svg': {
+          loaders: ['@svgr/webpack'],
+          as: '*.js',
+        },
+      },
+    },
+  },
+};
+
+export default nextConfig;
